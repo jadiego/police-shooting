0aaa0067dba372692bfec8ea231a71c642349308
John Diego <jadiego@uw.edu>
2015-10-12 13:08:43 -0700

    1st time creating map by editing drawMap function

--- js/map.js
+++ js/map.js
@@ -1,34 +1,34 @@
 // Function to draw your map
-var drawMap() = function() {
+var drawMap = function() {
+    
+    // Create map and set view
+    var map = L.map("mapSpace").setView([38.8833, 77.0167], center)
 
-  // Create map and set view
- 
+    // Create a tile layer variable using the appropriate url
+    var layer = L.tileLayer("https://api.mapbox.com/v4/mapbox.comic/0/0/0.png?access_token=pk.eyJ1IjoiamFkaWVnbyIsImEiOiJjaWZvY2VwNmdoNjNvc3htN3B2NW52bTlwIn0.7dCCLGVvoDjg3AHS8m5s0Q")
 
-  // Create a tile layer variable using the appropriate url
+    // Add the layer to your map
+    layer.addTo(map)
 
-
-  // Add the layer to your map
- 
-
-  // Execute your function to get data
+    // Execute your function to get data
  
 }
 
 // Function for getting data
 var getData = function() {
 
-  // Execute an AJAX request to get the data in data/response.js
+    // Execute an AJAX request to get the data in data/response.js
 
-
-  // When your request is successful, call your customBuild function
+    
+    // When your request is successful, call your customBuild function
 
 }
 
 // Loop through your data and add the appropriate layers and points
 var customBuild = function() {
-	// Be sure to add each layer to the map
+    // Be sure to add each layer to the map
 
-	// Once layers are on the map, add a leaflet controller that shows/hides layers
+    // Once layers are on the map, add a leaflet controller that shows/hides layers
   
 }
 
