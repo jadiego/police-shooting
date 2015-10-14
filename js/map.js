3b17ddec803dd144052614b1b556ba3ee0306ca0
John Diego <jadiego@uw.edu>
2015-10-13 17:03:42 -0700

    map works, getData doesnt

--- js/map.js
+++ js/map.js
@@ -3,21 +3,28 @@ var drawMap = function() {
     var map = L.map("map").setView([38.925,-96.724], 5);
     var layer = L.tileLayer("https://api.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiamFkaWVnbyIsImEiOiJjaWZvY2VwNmdoNjNvc3htN3B2NW52bTlwIn0.7dCCLGVvoDjg3AHS8m5s0Q");
     layer.addTo(map);
+    getData();
 }
+
 
 // Function for getting data
 var getData = function() {
-
-    // Execute an AJAX request to get the data in data/response.js
-
-    
-    // When your request is successful, call your customBuild function
-
-}
+    var data;
+    $.ajax({
+        url:"../data/response.json",
+        type: "get",
+        success:function(dat) {
+        data = dat
+        // Do something with your data!
+    }, 
+    dataType:"json"
+    }
+)}
 
 // Loop through your data and add the appropriate layers and points
 var customBuild = function() {
     // Be sure to add each layer to the map
-
+    
+    
     // Once layers are on the map, add a leaflet controller that shows/hides layers
 }
