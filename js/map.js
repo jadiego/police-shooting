1cf98185c06480922b38becb0347a75bd3b7cb37
John Diego <jadiego@uw.edu>
2015-10-13 21:13:03 -0700

    ajax request now works

--- js/map.js
+++ js/map.js
@@ -1,30 +1,26 @@
 // Function to draw map
 var drawMap = function() {
-    var map = L.map("map").setView([38.925,-96.724], 5);
-    var layer = L.tileLayer("https://api.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiamFkaWVnbyIsImEiOiJjaWZvY2VwNmdoNjNvc3htN3B2NW52bTlwIn0.7dCCLGVvoDjg3AHS8m5s0Q");
-    layer.addTo(map);
+    var map = L.map("map").setView([38.925, -96.724], 5);
+    var USLayer = L.tileLayer("https://api.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiamFkaWVnbyIsImEiOiJjaWZvY2VwNmdoNjNvc3htN3B2NW52bTlwIn0.7dCCLGVvoDjg3AHS8m5s0Q");
+    USLayer.addTo(map);
     getData();
 }
 
-
+var data;
 // Function for getting data
 var getData = function() {
-    var data;
     $.ajax({
-        url:"../data/response.json",
+        dataType: "json",
+        url: "../data/response.json",
         type: "get",
-        success:function(dat) {
-        data = dat
-        // Do something with your data!
-    }, 
-    dataType:"json"
-    }
-)}
+        success: function(dat) {
+            data = dat;
+        }
+    });
+    customBuild();
+}
 
-// Loop through your data and add the appropriate layers and points
+// Loops through data and adds the appropriate layers and points
 var customBuild = function() {
-    // Be sure to add each layer to the map
-    
-    
-    // Once layers are on the map, add a leaflet controller that shows/hides layers
+
 }
