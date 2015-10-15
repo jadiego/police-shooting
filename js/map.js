8ce661feccc2fe30a84d5e3b05c735d030385a0c
John Diego <jadiego@uw.edu>
2015-10-14 23:58:07 -0700

    points with popups are added to map

--- js/map.js
+++ js/map.js
@@ -1,26 +1,46 @@
 // Function to draw map
 var drawMap = function() {
-    var map = L.map("map").setView([38.925, -96.724], 5);
-    var USLayer = L.tileLayer("https://api.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiamFkaWVnbyIsImEiOiJjaWZvY2VwNmdoNjNvc3htN3B2NW52bTlwIn0.7dCCLGVvoDjg3AHS8m5s0Q");
-    USLayer.addTo(map);
-    getData();
+    map = L.map("map").setView([38.925, -96.724], 5);
+    var USLayer = L.tileLayer("https://api.mapbox.com/v4/mapbox.streets-basic/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiamFkaWVnbyIsImEiOiJjaWZvY2VwNmdoNjNvc3htN3B2NW52bTlwIn0.7dCCLGVvoDjg3AHS8m5s0Q").addTo(map);
+    getData();   
 }
 
-var data;
+
 // Function for getting data
 var getData = function() {
     $.ajax({
-        dataType: "json",
         url: "../data/response.json",
         type: "get",
         success: function(dat) {
             data = dat;
-        }
+            customBuild();
+        },
+        dataType: "json"
     });
-    customBuild();
 }
 
+var layers = {};
 // Loops through data and adds the appropriate layers and points
 var customBuild = function() {
-
+    for(var i = 0; i < data.length; i++) {
+        //Creates circle with popup
+        var circle = L.circle([data[i].lat, data[i].lng], 20000, {
+            color: (data[i]["Hit or Killed?"] === "Killed") ? "red" : "gray",
+            fillOpacity: 0.3
+        }).bindPopup(data[i].Summary + "Source".link(data[i]["Source Link"]));
+        // If data doesn't have "Race" key, then changes it to Unknown;
+        var personRace = ("Race" in data[i]) ? data[i].Race : "Unknown";
+        if (layers.hasOwnProperty(personRace)) {
+            layers[personRace].addLayer(circle);
+        } else {
+            layers[personRace] = L.layerGroup([circle]);   
+        }
+    }
+    $.each(layers, function(key, value) {
+        if (layers.hasOwnProperty(key)) {
+            this.addTo(map);
+        }
+    
+    });
+    L.control.layers(null,layers).addTo(map);
 }
